fb6f1014daa9def7d41a911e07fb8b35520a5a4d
anshika200504 <anshiii.0104@gmail.com>
2024-04-08 12:46:36 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,12 @@
+x=int(input("enter a number"))
+y=int(input("enter a number"))
+z=int(input("enter a number"))
+print("sum is",x+y+z)
+print("subtract is",x-y-z)
+print("divisibilityis",x/y)
+print("doble codes",x//y)
+print("per is",x+y+z/200*100)
+print("vol is",x*y*z)
+print("square is",x**2)
+
+
