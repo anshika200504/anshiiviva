af98b7d89d8a1d9e189206500a2bec9361f329b0
anshika200504 <158558581+anshika200504@users.noreply.github.com>
2024-04-08 12:51:15 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,7 +1,7 @@
 x=int(input("enter a number"))
 y=int(input("enter a number"))
 z=int(input("enter a number"))
-print("sum is",x+y+z)
+print("sum is",x+y+o)
 print("subtract is",x-y-z)
 print("divisibilityis",x/y)
 print("doble codes",x//y)
